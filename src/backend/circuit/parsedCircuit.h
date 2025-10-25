398ffbee183e84706c3444e38ee4eeb0ceb7b00f
niknal357 <46793548+niknal357@users.noreply.github.com>
2025-10-26 00:11:18 +0200

    hashes correctly?

--- src/backend/circuit/parsedCircuit.h
+++ src/backend/circuit/parsedCircuit.h
@@ -146,8 +146,10 @@ public:
 private:
 	struct ConnectionHash {
 		size_t operator()(const ParsedCircuit::ConnectionData& p) const {
-			return std::hash<block_id_t>()(p.outputEndId) ^ std::hash<block_id_t>()(p.outputEndId) ^
-				std::hash<connection_end_id_t>()(p.outputBlockId) ^ std::hash<connection_end_id_t>()(p.inputBlockId);
+			return std::hash<block_id_t>()(p.outputBlockId) ^
+				std::hash<connection_end_id_t>()(p.outputEndId) ^
+				std::hash<block_id_t>()(p.inputBlockId) ^
+				std::hash<connection_end_id_t>()(p.inputEndId);
 		}
 	};
 
