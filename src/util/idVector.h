9802a6e60117b26d6f6535382dbf77a4b0057cf8
niknal357 <46793548+niknal357@users.noreply.github.com>
2025-10-26 11:39:43 +0100

    update idvector

--- src/util/idVector.h
+++ src/util/idVector.h
@@ -14,77 +14,64 @@ public:
     static_assert(std::is_integral_v<rep>, "Id::rep must be an integral type");
 
     constexpr IdVector() = default;
-    explicit IdVector(id_type n)                  : storage_(to_index_(n)) {}
-    IdVector(id_type n, const T& value)           : storage_(to_index_(n), value) {}
+    explicit IdVector(id_type n)                  : storage_(rep(n)) {}
+    IdVector(id_type n, const T& value)           : storage_(rep(n), value) {}
 
-    id_type size() const noexcept                { return id_type{to_rep_(storage_.size())}; }
-    bool    empty() const noexcept               { return storage_.empty(); }
+    inline id_type size() const noexcept                { return id_type{rep(storage_.size())}; }
+    inline bool    empty() const noexcept               { return storage_.empty(); }
 
-    id_type capacity() const noexcept            { return id_type{to_rep_(storage_.capacity())}; }
+    inline id_type capacity() const noexcept            { return id_type{rep(storage_.capacity())}; }
 
-    void reserve(id_type n)                      { storage_.reserve(to_index_(n)); }
-    void resize(id_type n)                       { storage_.resize(to_index_(n)); }
-    void resize(id_type n, const T& value)       { storage_.resize(to_index_(n), value); }
-    void resizeWithOffset(id_type n, int offset) { storage_.resize(to_index_(n) + offset); }
-    void resizeWithOffset(id_type n, int offset, const T& value) { storage_.resize(to_index_(n) + offset, value); }
+    inline void reserve(id_type n)                      { storage_.reserve(n.get()); }
+    inline void resize(id_type n)                       { storage_.resize(n.get()); }
+    inline void resize(id_type n, const T& value)       { storage_.resize(n.get(), value); }
+    inline void resizeWithOffset(id_type n, int offset) { storage_.resize(n.get() + offset); }
+    inline void resizeWithOffset(id_type n, int offset, const T& value) { storage_.resize(n.get() + offset, value); }
 
-    void clear() noexcept                        { storage_.clear(); }
+    inline void clear() noexcept                        { storage_.clear(); }
 
-    T& operator[](id_type id)                    { return storage_[to_index_(id)]; }
-    const T& operator[](id_type id) const        { return storage_[to_index_(id)]; }
+    inline T& operator[](id_type id)                    { return storage_[id.get()]; }
+    inline const T& operator[](id_type id) const        { return storage_[id.get()]; }
 
-    T& at(id_type id)                            { return storage_.at(to_index_(id)); }
-    const T& at(id_type id) const                { return storage_.at(to_index_(id)); }
+    inline T& at(id_type id)                            { return storage_.at(id.get()); }
+    inline const T& at(id_type id) const                { return storage_.at(id.get()); }
 
-    id_type push_back(const T& v) {
+    inline id_type push_back(const T& v) {
         storage_.push_back(v);
         return last_id_();
     }
-    id_type push_back(T&& v) {
+    inline id_type push_back(T&& v) {
         storage_.push_back(std::move(v));
         return last_id_();
     }
     template<class... Args>
-    id_type emplace_back(Args&&... args) {
+    inline id_type emplace_back(Args&&... args) {
         storage_.emplace_back(std::forward<Args>(args)...);
         return last_id_();
     }
 
-    id_type begin_id() const noexcept { return id_type(0); }
-    id_type end_id()   const noexcept { return size(); }
+    inline id_type begin_id() const noexcept { return id_type(0); }
+    inline id_type end_id()   const noexcept { return size(); }
 
-    bool contains(id_type id) const noexcept {
-        auto i = to_index_(id);
-        return i < storage_.size();
+    inline bool contains(id_type id) const noexcept {
+        return id.get() < storage_.size();
     }
 
-    IdRange<tag, rep> ids() const {
+    inline IdRange<tag, rep> ids() const {
         return range(begin_id(), end_id());
     }
 
-    auto begin() noexcept { return storage_.begin(); }
-    auto end()   noexcept { return storage_.end(); }
-    auto begin() const noexcept { return storage_.begin(); }
-    auto end()   const noexcept { return storage_.end(); }
-    const std::vector<T>& raw() const noexcept { return storage_; }
-    std::vector<T>&       raw()       noexcept { return storage_; }
+    inline auto begin() noexcept { return storage_.begin(); }
+    inline auto end()   noexcept { return storage_.end(); }
+    inline auto begin() const noexcept { return storage_.begin(); }
+    inline auto end()   const noexcept { return storage_.end(); }
+    inline const std::vector<T>& raw() const noexcept { return storage_; }
+    inline std::vector<T>&       raw()       noexcept { return storage_; }
 
 private:
-    static std::size_t to_index_(id_type id) {
-        if constexpr (std::is_signed_v<rep>) {
-            auto v = id.get();
-            return v < 0 ? static_cast<std::size_t>(0) : static_cast<std::size_t>(v);
-        } else {
-            return static_cast<std::size_t>(id.get());
-        }
-    }
-    static rep to_rep_(std::size_t i) {
-        return static_cast<rep>(i);
-    }
-
-    id_type last_id_() const {
+    inline id_type last_id_() const {
         auto n = storage_.size();
-        return id_type{to_rep_(n - 1)};
+        return id_type{rep(n - 1)};
     }
 
     std::vector<T> storage_;
