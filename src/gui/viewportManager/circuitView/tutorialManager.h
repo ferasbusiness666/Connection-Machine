3d7fa1aa6496d3fe65135dd1f07928a36819dce9
conman4562 <ckx1138@gmail.com>
2025-11-07 19:52:54 -0500

    framework for modular tutorials

--- src/gui/viewportManager/circuitView/tutorialManager.h
+++ src/gui/viewportManager/circuitView/tutorialManager.h
@@ -7,35 +7,70 @@ class Environment;
 #include "renderer/elementCreator.h"
 #include "./viewManager/viewManager.h"
 
+struct TutorialCondition {
+	struct BlockRequirement {
+		Position pos;
+		BlockType type;
+	};
+	struct ConnectionRequirement {
+		Position pos1;
+		Position pos2;
+	};
+	struct LogicStateRequirement {
+		Position pos;
+		logic_state_t state;
+		int numSteps;
+	};
+	std::vector<BlockRequirement> blocks;
+	std::vector<ConnectionRequirement> connections;
+	std::vector<LogicStateRequirement> logicStates;
+};
+
+struct TutorialAction {
+	struct BlockPreviewInfo {
+		Position pos;
+		BlockType type;
+		Orientation orientation;
+	};
+	struct ConnectionPreviewInfo {
+		Position pos1;
+		Position pos2;
+	};
+	std::vector<std::string> messages;
+	std::vector<BlockPreviewInfo> blockPreviews;
+	std::vector<ConnectionPreviewInfo> connectionPreviews;
+};
+
+struct TutorialStep {
+	TutorialCondition condition;
+	TutorialAction action;
+};
+
 class TutorialManager {
 public:
 	TutorialManager(Environment& environment, CircuitView& circuitView);
 	void StartTutorial();
 	void Stop();
-
+	void setTutorial(const std::vector<TutorialStep>& steps);
 
 private:
 	void checkTutorial(DifferenceSharedPtr, circuit_id_t);
 	void checkTutorialState(Position pos, bool state);
 
-	void basicTutorial();
-	void basicTutorialPart1();
-	void basicTutorialPart2();
-	void basicTutorialPart3();
-	void basicTutorialPart4();
-	void basicTutorialPart5();
-	void basicTutorialPart6();
-
-	SharedEvaluator evaluator;
-	SharedCircuit curentCircuit;
+	void runCurrentStep();
+    bool isCurrentStepComplete() const;
+    void advanceTutorial();
 
 	CircuitView* circuitView;
-	ViewManager viewManager;
-	Environment& environment;
 	ElementCreator elementCreator;
+	Environment& environment;
+	SharedEvaluator evaluator;
+	SharedCircuit curentCircuit;
+	ViewManager viewManager;
 
-	int tutorialState;
 	bool tutorialRunning;
+	int tutorialState;
+	std::vector<TutorialStep> tutorialSteps;
 };
 
 #endif
