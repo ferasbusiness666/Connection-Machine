577c7b1b5ad74b46b3f83da9a0ee7e6af6d60a88
niknal357 <46793548+niknal357@users.noreply.github.com>
2025-11-19 10:14:31 +0100

    Add resource copying logic to tests (incl. copying circuits that can be used for tests)

--- cmake/makeTests.cmake
+++ cmake/makeTests.cmake
@@ -50,3 +50,51 @@ target_compile_definitions(${PROJECT_NAME}_tests PRIVATE "PROJECT_VERSION=\"${PR
 target_link_libraries(${PROJECT_NAME}_tests PRIVATE ${EXTERNAL_LINKS})
 
 target_precompile_headers(${PROJECT_NAME}_tests PRIVATE "${SOURCE_DIR}/precompiled.h")
+
+set(TEST_RESOURCES_BINARY_DIR "${CMAKE_CURRENT_BINARY_DIR}/resources")
+
+file(GLOB_RECURSE TEST_RESOURCE_FILES RELATIVE "${RESOURCES_DIR}" "${RESOURCES_DIR}/*")
+set(TEST_RESOURCE_PRODUCTS)
+foreach(resource_path_relative IN LISTS TEST_RESOURCE_FILES)
+	set(original_resource "${RESOURCES_DIR}/${resource_path_relative}")
+	set(copied_resource "${TEST_RESOURCES_BINARY_DIR}/${resource_path_relative}")
+	get_filename_component(copied_resource_dir "${copied_resource}" DIRECTORY)
+
+	add_custom_command(
+		OUTPUT "${copied_resource}"
+		COMMAND "${CMAKE_COMMAND}" -E make_directory "${copied_resource_dir}"
+		COMMAND "${CMAKE_COMMAND}" -E copy_if_different "${original_resource}" "${copied_resource}"
+		DEPENDS "${original_resource}"
+		COMMENT "Copying resource for tests: ${resource_path_relative}"
+		VERBATIM
+	)
+
+	list(APPEND TEST_RESOURCE_PRODUCTS "${copied_resource}")
+endforeach()
+
+set(TEST_CIRCUITS_SOURCE_DIR "${CMAKE_SOURCE_DIR}/tests/circuits")
+set(TEST_CIRCUIT_PRODUCTS)
+if(EXISTS "${TEST_CIRCUITS_SOURCE_DIR}")
+	file(GLOB_RECURSE TEST_CIRCUIT_FILES RELATIVE "${TEST_CIRCUITS_SOURCE_DIR}" "${TEST_CIRCUITS_SOURCE_DIR}/*")
+	foreach(circuit_path_relative IN LISTS TEST_CIRCUIT_FILES)
+		set(original_circuit "${TEST_CIRCUITS_SOURCE_DIR}/${circuit_path_relative}")
+		set(copied_circuit "${TEST_RESOURCES_BINARY_DIR}/circuits/${circuit_path_relative}")
+		get_filename_component(copied_circuit_dir "${copied_circuit}" DIRECTORY)
+
+		add_custom_command(
+			OUTPUT "${copied_circuit}"
+			COMMAND "${CMAKE_COMMAND}" -E make_directory "${copied_circuit_dir}"
+			COMMAND "${CMAKE_COMMAND}" -E copy_if_different "${original_circuit}" "${copied_circuit}"
+			DEPENDS "${original_circuit}"
+			COMMENT "Copying test circuit: ${circuit_path_relative}"
+			VERBATIM
+		)
+
+		list(APPEND TEST_CIRCUIT_PRODUCTS "${copied_circuit}")
+	endforeach()
+else()
+	message(WARNING "Test circuits directory not found: ${TEST_CIRCUITS_SOURCE_DIR}")
+endif()
+
+add_custom_target(copy-test-resources DEPENDS ${TEST_RESOURCE_PRODUCTS} ${TEST_CIRCUIT_PRODUCTS})
+add_dependencies(${PROJECT_NAME}_tests copy-test-resources)
