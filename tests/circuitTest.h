8ef8749f00882d13f46f67369022e47f9b9a1971
ItchyTrack <benherman345@gmail.com>
2025-11-20 17:11:16 -0500

    GetNonExistingObjects

--- tests/circuitTest.h
+++ tests/circuitTest.h
@@ -1,14 +1,14 @@
 #ifndef circuitTests_h
 #define circuitTests_h
 
-#include <gtest/gtest.h>
 #include "environment/environment.h"
+#include <gtest/gtest.h>
 
 class CircuitTest : public ::testing::Test {
 protected:
 	void SetUp() override;
 	void TearDown() override;
-	Environment environment {false};
+	Environment environment{ false };
 	SharedCircuit circuit = nullptr;
 	int i;
 };
---
+++ tests/getNonExistingObjects.h
@@ -0,0 +1,8 @@
+#ifndef GetNonExistingObjects_h
+#define GetNonExistingObjects_h
+
+#include <gtest/gtest.h>
+
+class GetNonExistingObjects : public ::testing::Test {};
+
+#endif /* GetNonExistingObjects_h */
